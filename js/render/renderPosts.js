87a166e02e4945ddbf46db915f129be24a9b6e06
Simen Magnussen <126103447+Pockitt@users.noreply.github.com>
2024-05-01 17:09:11 +0200

    Rendering posts

--- js/render/renderPosts.js
+++ js/render/renderPosts.js
@@ -3,13 +3,17 @@ export async function renderPosts(posts, parent, heading) {
     parent.innerHTML = "";
     parent.innerHTML = heading;
     for (let i = 0; i < posts.length; i++) {
-      parent.innerHTML += ` <div class="post">
-      <figure>
+      parent.innerHTML += ` 
+      <div class="post">
+        <figure class="home-page-img-container">
+          <a href="./Post/index.html?id=${posts[i].id}">
+            <img class="home-page-img" src="${posts[i].images[0].src}" alt="picture of ${posts[i].name}" />
+          </a>
+        </figure>
         <a href="./Post/index.html?id=${posts[i].id}">
-          <img src="${posts[i].images[0].src}" alt="picture of ${posts[i].name}" />
           <h2 class="post-title">${posts[i].name}</h2>
         </a>
-      </figure>
-    </div> `
+        <p class="post-preview"></p>
+      </div> `
     }
   }
