ab3a7dbe7e907a32513b89e04b03d8a86a487008
Simen Magnussen <simen.magnussen@outlook.com>
2024-05-07 14:30:27 +0200

    Lots of styling on point
    Need to get that carousel up

--- js/pages/postPage.js
+++ js/pages/postPage.js
@@ -1,6 +1,6 @@
 import { renderPost } from "../render/renderPost.js";
 
-export const postPageParent = document.querySelector ("section");
+export const postPageParent = document.querySelector (".post-section");
 export const postPageTitle = document.querySelector ("title");
 
 export  function renderPostPage (postPageParent, postPageTitle) {
--- js/render/renderPost.js
+++ js/render/renderPost.js
@@ -1,15 +1,15 @@
 export function renderPost(post) {
-    const postPage = document.querySelector ("section");
+    const postPage = document.querySelector (".post-section");
     const title = document.querySelector ("title");
     postPage.innerHTML = "";
     postPage.innerHTML += `
-    <figure>
+    <figure class="post-main-img-container">
       <img class="post-main-img" src="${post.images[0].src}" />
     </figure>
-    <h2>${post.name}</h2>
-    <p>
+    <h2 class="post-h2">${post.name}</h2>
+    <p class="post-p">
       ${post.description}
     </p>` 
       
-      title.innerHTML += `Plant Dads | ${post.name}`
+      title.innerHTML += ` | ${post.name}`
 }
